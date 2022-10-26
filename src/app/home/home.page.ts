34cf6fdc4d47ca12286b72fdf8485a2c610eb75f
victor <vmorel812@gmail.com>
2022-10-26 13:53:44 -0300

    Tarea hola mundo ionic

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  nombre: string = '';
+
+  constructor(
+    private controladorToast: ToastController
+  ) {}
+
+  async saludar(){
+    const toast = await this.controladorToast.create({
+      message: 'Hola '+this.nombre+'!',
+      duration: 3000,
+      position: 'bottom'
+    });
+    await  toast.present();
+  }
+
+}
